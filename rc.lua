2bc8cb3581cc2b86d6154e5eacab5cbfb26e405c
Guillaume Dott <guillaume+github@dott.fr>
2015-05-20 11:40:13 +0200

    Add rules again

--- rc.lua
+++ rc.lua
@@ -568,6 +568,20 @@ globalkeys = awful.util.table.join(
 root.keys(globalkeys)
 -- }}}
 
+-- {{{ Rules
+-- Rules to apply to new clients (through the "manage" signal).
+awful.rules.rules = {
+    -- All clients will match this rule.
+    { rule = { },
+      properties = { border_width = beautiful.border_width,
+                     border_color = beautiful.border_normal,
+                     focus = awful.client.focus.filter,
+                     raise = true,
+                     keys = clientkeys,
+                     buttons = clientbuttons } },
+}
+-- }}}
+
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
 client.connect_signal("manage", function (c, startup)
